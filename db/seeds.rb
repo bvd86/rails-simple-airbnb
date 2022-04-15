5ae50714f8f8e85bc96154360706888fbd634465
Billy Veillette-Daigle <bvd86@icloud.com>
2022-04-14 22:34:51 -0400

    Updating Simple AirBnB - Front-end not done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning the DB..."
+Flat.destroy_all
+
+puts "Creating flats..."
+4.times do
+  Flat.create!(
+    name: Faker::Games::Pokemon.location,
+    address: Faker::Address.street_address,
+    description: Faker::TvShows::RuPaul.quote,
+    price_per_night: (100..500).to_a.sample,
+    number_of_guests: (1..10).to_a.sample
+  )
+end
+puts "Done"
